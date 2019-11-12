f98edc7c93568abbb6ab55807259dcedf514b12c
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-13 00:48:55 +0300

    Initiation

--- trn11com_integration/api.py
+++ trn11com_integration/api.py
@@ -2,6 +2,23 @@ import frappe
 from zeep import Client, Settings
 
 
+def communicate_with_n11com(servicepath):
+    company = frappe.defaults.get_user_default("Company")
+    # This should only be used if the integration is enabled
+    if frappe.db.get_single_value("TR n11com Integration Settings", "enable") == 1:
+        # Select the server according to the mode of the integration
+        soapserver = frappe.db.get_single_value("TR n11com Integration Settings", "server")
+        soapservicepath = servicepath
+
+        auth = [{
+            'appKey': frappe.db.get_value("TR n11com Company Settings", "company", "appkey"),
+            'appSecret': frappe.db.get_value("TR n11com Company Settings", "company", "appsecret")
+        }]
+        wsdl = soapserver + soapservicepath
+        settings = Settings(strict=False, raw_response=False)
+        return Client(wsdl, settings=settings)
+
+
 @frappe.whitelist()
 def test_integration(testappkey, testappsecret):
     soapserver = frappe.db.get_single_value("TR n11com Integration Settings", "server")
@@ -9,14 +26,14 @@ def test_integration(testappkey, testappsecret):
 
     wsdl = soapserver + soapservicepath
     settings = Settings(strict=False, raw_response=False)
-    client = Client(str(wsdl), settings=settings)
+    client = Client(wsdl, settings=settings)
 
     # company = frappe.defaults.get_user_default("Company")
     auth = [{
         # 'appKey': frappe.db.get_value("TR n11com Company Settings", "company", "appkey"),
-        'appKey': str(testappkey),
+        'appKey': testappkey,
         # 'appSecret': frappe.db.get_value("TR n11com Company Settings", "company", "appsecret")
-        'appSecret': str(testappsecret)
+        'appSecret': testappsecret
     }]
 
     return client.service.GetTopLevelCategories(auth).result.status
