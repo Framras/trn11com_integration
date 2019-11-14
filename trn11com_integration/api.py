3cca174d58c5781120f0d4cb697138c67b028113
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-15 01:18:44 +0300

    Initiation

--- trn11com_integration/api.py
+++ trn11com_integration/api.py
@@ -2,38 +2,90 @@ import frappe
 from zeep import Client, Settings
 
 
-def communicate_with_n11com(servicepath):
+def get_n11com_auth(servicecategory):
     company = frappe.defaults.get_user_default("Company")
-    # This should only be used if the integration is enabled
-    if frappe.db.get_single_value("TR n11com Integration Settings", "enable") == 1:
-        # Select the server according to the mode of the integration
-        soapserver = frappe.db.get_single_value("TR n11com Integration Settings", "server")
-        soapservicepath = servicepath
+    # This should only be used if the integration is enabled for the company
+    if frappe.db.get_value("TR n11com Company Settings", company, "enable") == 1:
+        factory = get_n11com_client(servicecategory).type_factory(
+            frappe.db.get_single_value("TR n11com Integration Settings", "default_namespace"))
+        return factory.Authentication(appKey=frappe.db.get_value("TR n11com Company Settings", "company", "appkey"),
+                                      appSecret=frappe.db.get_value("TR n11com Company Settings", "company",
+                                                                    "appsecret"))
+    else:
+        return ""
 
-        auth = [{
-            'appKey': frappe.db.get_value("TR n11com Company Settings", "company", "appkey"),
-            'appSecret': frappe.db.get_value("TR n11com Company Settings", "company", "appsecret")
-        }]
-        wsdl = soapserver + soapservicepath
-        settings = Settings(strict=False, raw_response=False)
+
+def get_n11com_client(servicecategory):
+    company = frappe.defaults.get_user_default("Company")
+    # This should only be used if the integration is enabled for the company
+    if frappe.db.get_value("TR n11com Company Settings", company, "enable") == 1:
+        strictsetting = False
+        if frappe.db.get_single_value("TR n11com Integration Settings", "strict") == 1:
+            strictsetting = True
+        rawresponsesetting = False
+        if frappe.db.get_single_value("TR n11com Integration Settings", "raw_response") == 1:
+            rawresponsesetting = True
+
+        wsdl = frappe.db.get_single_value("TR n11com Integration Settings", "server") + get_n11com_servicepath(
+            servicecategory)
+        settings = Settings(strict=strictsetting, raw_response=rawresponsesetting)
         return Client(wsdl, settings=settings)
+    else:
+        return ""
+
+
+def get_n11com_typefactory(servicecategory):
+    # Available types are:
+    # CategoryData, SubCategoryData, SubCategory, ParentCategoryData, ParentCategory, CategoryAttributeList,
+    # TopCategoryList, SubCategoryList, CategoryProductAttributeList, CategoryProductAttributeData,
+    # CategoryProductAttributeValueData, CategoryProductAttributeValueList, CategoryAttributeData,
+    # CategoryAttributeValueList, CategoryAttributeValueData, PagingData, RequestPagingData, BaseRequest,
+    # Authentication, BaseResponse, ResultInfo
+    return get_n11com_client(servicecategory).type_factory(
+        frappe.db.get_single_value("TR n11com Integration Settings", "default_namespace"))
+
+
+def get_n11com_service(servicecategory):
+    service = ""
+    port = ""
+    if servicecategory == "Category":
+        service = "CategoryServicePortService"
+        port = "CategoryServicePortSoap11"
+    elif servicecategory == "Product":
+        service = "ProductServicePortService"
+        port = "ProductServicePortSoap11"
+    elif servicecategory == "Order":
+        service = "OrderServicePortService"
+        port = "OrderServicePortSoap11"
+
+    return get_n11com_client(servicecategory).bind(service, port)
+
+
+def get_n11com_servicepath(servicecategory):
+    if servicecategory == "Category":
+        return "/ws/CategoryService.wsdl"
+    elif servicecategory == "Product":
+        return "/ws/ProductService.wsdl"
+    elif servicecategory == "Order":
+        return "/ws/OrderService.wsdl"
+    else:
+        return ""
 
 
 @frappe.whitelist()
 def test_integration(testappkey, testappsecret):
-    soapserver = frappe.db.get_single_value("TR n11com Integration Settings", "server")
-    soapservicepath = "/ws/CategoryService.wsdl"
+    strictsetting = False
+    if frappe.db.get_single_value("TR n11com Integration Settings", "strict") == 1:
+        strictsetting = True
+    rawresponsesetting = False
+    if frappe.db.get_single_value("TR n11com Integration Settings", "raw_response") == 1:
+        rawresponsesetting = True
 
-    wsdl = soapserver + soapservicepath
-    settings = Settings(strict=False, raw_response=False)
-    client = Client(wsdl, settings=settings)
+    wsdl = frappe.db.get_single_value("TR n11com Integration Settings", "server") + get_n11com_servicepath(
+        "Category")
+    settings = Settings(strict=strictsetting, raw_response=rawresponsesetting)
+    factory = Client(wsdl, settings=settings).type_factory(
+        frappe.db.get_single_value("TR n11com Integration Settings", "default_namespace"))
+    auth = factory.Authentication(appKey=testappkey, appSecret=testappsecret)
 
-    # company = frappe.defaults.get_user_default("Company")
-    auth = [{
-        # 'appKey': frappe.db.get_value("TR n11com Company Settings", "company", "appkey"),
-        'appKey': testappkey,
-        # 'appSecret': frappe.db.get_value("TR n11com Company Settings", "company", "appsecret")
-        'appSecret': testappsecret
-    }]
-
-    return client.service.GetTopLevelCategories(auth).result.status
+    return get_n11com_service("Category").GetTopLevelCategories(auth).result.status
---
+++ trn11com_integration/sil.py
@@ -0,0 +1,112 @@
+from zeep import Client, Settings
+
+
+def get_n11com_auth(servicecategory):
+    appkey = "df848c60-6f80-473b-b129-a00f379a21f1"
+    appsecret = "lkuFEB2CxA5cUdun"
+
+    return get_n11com_typefactory(servicecategory).Authentication(appKey=appkey, appSecret=appsecret)
+
+
+def get_n11com_client(servicecategory):
+    soapserver = "https://api.n11.com"
+    strictsetting = False
+    rawresponsesetting = False
+
+    wsdl = soapserver + get_n11com_servicepath(servicecategory)
+    settings = Settings(strict=strictsetting, raw_response=rawresponsesetting)
+    return Client(wsdl, settings=settings)
+
+
+def get_n11com_typefactory(servicecategory):
+    defaultnamespace = "http://www.n11.com/ws/schemas"
+    return get_n11com_client(servicecategory).type_factory(defaultnamespace)
+
+
+def get_n11com_service(servicecategory):
+    service = ""
+    port = ""
+    if servicecategory == "Category":
+        service = "CategoryServicePortService"
+        port = "CategoryServicePortSoap11"
+    elif servicecategory == "Product":
+        service = "ProductServicePortService"
+        port = "ProductServicePortSoap11"
+    elif servicecategory == "Order":
+        service = "OrderServicePortService"
+        port = "OrderServicePortSoap11"
+
+    return get_n11com_client(servicecategory).bind(service, port)
+
+
+def get_n11com_servicepath(servicecategory):
+    if servicecategory == "Category":
+        return "/ws/CategoryService.wsdl"
+    elif servicecategory == "Product":
+        return "/ws/ProductService.wsdl"
+    elif servicecategory == "Order":
+        return "/ws/OrderService.wsdl"
+    else:
+        return ""
+
+
+sellercode = "framras"
+
+# GetTopLevelCategories(auth: ns0:Authentication)
+# -> result: ns0:ResultInfo, categoryList: ns0:TopCategoryList
+response = get_n11com_service("Category").GetTopLevelCategories(get_n11com_auth("Category"))
+# ns0:TopCategoryList(category: ns0:SubCategory[])
+# ns0:SubCategory(id: xsd:long, name: xsd:string)
+# if rawresponsesetting:
+#     print("1:" + response.text)
+#     print("1:" + "not implemented")
+# else:
+print("1:" + response.result.status)
+print(response["result"])
+print(response["categoryList"])
+for r in response.categoryList.category:
+    print(r.id)
+    print(r.name)
+
+response = get_n11com_service("Product").GetProductBySellerCode(get_n11com_auth("Product"), sellerCode="RHW003301")
+# if rawresponsesetting:
+#     print("2:" + response.text)
+#     print("2:" + "not implemented")
+# else:
+print("2:" + response.result.status)
+print("2:" + str(response))
+
+# ns0:OrderSearchPeriod(startDate: xsd:string, endDate: xsd:string)
+
+# ns0:OrderDataListRequest(productId: xsd:long, status: xsd:string, buyerName: xsd:string, orderNumber: xsd:string,
+# productSellerCode: xsd:string, recipient: xsd:string, sameDayDelivery: xsd:boolean, period: ns0:OrderSearchPeriod,
+# sortForUpdateDate: xsd:boolean)
+searchData = {'productID': 213386280,
+              'buyerName': "",
+              'orderNumber': "",
+              'productSellerCode': "",
+              'recipient': "",
+              'period': {'startDate': "", 'endDate': ""},
+              'sortForUpdateDate': ""}
+
+orderdatalistrequest = get_n11com_client("Order").get_type("ns0:OrderDataListRequest")
+searchData3 = orderdatalistrequest(searchData)
+
+auth = get_n11com_auth("Order")
+
+# DetailedOrderList(auth: ns0:Authentication, searchData: ns0:OrderDataListRequest, pagingData: ns0:PagingData)
+# -> result: ns0:ResultInfo, orderList: ns0:DetailedOrderDataList, pagingData: ns0:PagingData
+response = get_n11com_service("Order").DetailedOrderList(auth, searchData)
+# ns0:ResultInfo(status: xsd:string, errorCode: xsd:string, errorMessage: xsd:string, errorCategory: xsd:string)
+
+# ns0:DetailedOrderDataList(order: ns0:DetailedOrderData[])
+# ns0:DetailedOrderData(id: xsd:long, invoiceType: xsd:string, status: xsd:integer, orderNumber: xsd:string,
+# totalAmount: xsd:decimal, paymentType: xsd:integer, citizenshipId: xsd:string,
+# orderItemList: ns0:DetailedOrderItemDataList, createDate: xsd:string)
+# if rawresponsesetting:
+#     print("3:" + response.text)
+#     print("3:" + "not implemented")
+# else:
+print("3:" + response.result.status)
+# ns3:DetailedOrderListResponse
+print(response.orderList)
